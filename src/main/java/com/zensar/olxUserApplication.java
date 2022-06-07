bb91d25b0ad0658774e95573cdc3b00d88c256d1
bhatrajusurya <104192366+bhatrajusurya@users.noreply.github.com>
2022-06-06 22:44:03 -0700

    Add files via upload

---
+++ src/main/java/com/zensar/olxUserApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class olxUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(olxUserApplication.class, args);
+	}
+
+}
